f438478c7ba640d54ab3a29d5e21d23a36dee734
vadimus0832 <33825877+vadimus0832@users.noreply.github.com>
2017-11-21 23:06:30 +0300

    Add files via upload

---
+++ Bike.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager
+{
+    class Bike : Transport
+    {
+        protected int AmountSpeeds;
+        protected bool Amortization;
+        protected string bName;
+      
+
+        public Bike(int Comfort, int AmountWheels, int AmountPlaces, int MaxSpeed, int ServiceCost, int MaxCapcity, int Size, string Color, int Weight, int Tax, string TransportType, int ReleaseIn)
+        { }
+
+        public Bike()
+        {
+        }
+    }
+}
