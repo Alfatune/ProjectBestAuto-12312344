d8ffef101a779daf172e6c7d23db8c97a9ec4ace
vadimus0832 <33825877+vadimus0832@users.noreply.github.com>
2017-11-21 23:31:21 +0300

    Add files via upload

---
+++ Scooter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager
+{
+    class Scooter : Transport
+    {
+        protected internal bool Folding;
+        protected internal bool Engine;
+        protected internal string sName;
+
+        public Scooter(int Comfort, int AmountWheels, int AmountPlaces, int MaxSpeed,
+             int Price ,int ServiceCost, int MaxCapcity, int Size, string Color, int Weight, int Tax, string TransportType, int ReleaseIn)
+        { }
+
+        
+    }
+}
