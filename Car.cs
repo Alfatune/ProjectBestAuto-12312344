77c58b597669bf152367be7e6f49c16d21373f63
vadimus0832 <33825877+vadimus0832@users.noreply.github.com>
2017-11-21 23:01:53 +0300

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager
+{
+    class Car : Transport
+    {
+        protected string locatedHelm;
+        protected int AmountDoors;
+        protected string cName;
+
+
+        public Car(int Comfort, int AmountWheels, int AmountPlaces, int MaxSpeed, int ServiceCost, int MaxCapcity, int Size, string Color, int Weight, int Tax, string TransportType, int ReleaseIn, int Price)
+        { }
+
+        public Car()
+        {
+        }
+    }
+}
