3bf36ebb6f758652b176053bce296f391177cfb8
vadimus0832 <33825877+vadimus0832@users.noreply.github.com>
2017-11-21 23:21:26 +0300

    Add files via upload

---
+++ MotorBike.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager
+{
+    class MotorBike : Transport
+    {
+        protected internal bool carriage;
+
+        protected internal string mName;
+
+
+        public MotorBike(int Comfort, int AmountWheels, int AmountPlaces, int MaxSpeed, int ServiceCost, int MaxCapcity,
+            int Size, int Price, string Color, int Weight, int Tax, string TransportType, int ReleaseIn)
+        { }
+
+        public MotorBike()
+        {
+        }
+    }
+}
