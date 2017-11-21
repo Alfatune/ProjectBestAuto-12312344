cee7a9fe49a16d098547323fd8982587e16a3527
vadimus0832 <33825877+vadimus0832@users.noreply.github.com>
2017-11-21 23:36:04 +0300

    Add files via upload

---
+++ Plane.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager
+{
+    class Plane : Transport
+    {
+        protected internal string pName;
+        protected internal int AmountEngine;
+        protected internal int AmountWings;
+
+        public Plane(int Comfort, int AmountWheels, int AmountPlaces, int MaxSpeed, 
+            int ServiceCost, int MaxCapcity, int Size, string Color, int Weight, int Tax, string TransportType, int ReleaseIn,int Price)
+        { }
+
+        public Plane()
+        {
+        }
+    }
+}
