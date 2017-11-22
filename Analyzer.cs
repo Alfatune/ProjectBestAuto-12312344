a33426f46fce29b041f8c15cfe6c9eff9ec4fa20
vadimus0832 <33825877+vadimus0832@users.noreply.github.com>
2017-11-23 00:12:49 +0300

    Add files via upload

---
+++ Analyzer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Analyzer : Person
+    {
+        protected bool AccessCar;
+        protected bool AccessBike;
+        protected bool AccessPlane;
+        protected bool AccessMotorBike;
+        protected bool AccessScooter;
+
+
+        public void Check(Person z)
+        {
+            Console.WriteLine("Процесс анализации...");
+            if (z.MedReference == true & z.DriverLicense == true & z.Age > 18 & z.Age < 80)
+            {
+                this.AccessCar = true;
+            }
+            if (z.MedReference == true & z.DriverLicense == true & z.Age > 18 & z.Age < 60)
+            {
+                this.AccessPlane = true;
+            }
+            if (z.MedReference == true & z.DriverLicense == true & z.Age > 16 & z.Age < 80)
+            {
+                this.AccessMotorBike = true;
+            }
+            if ((z.MedReference == true || z.MedReference == false) & (z.DriverLicense == true || z.DriverLicense == false) & (z.Age > 5 & z.Age < 75))
+            {
+                this.AccessBike = true;
+            }
+            if ((z.MedReference == true || z.MedReference == false) & (z.DriverLicense == true || z.DriverLicense == false) & (z.Age > 4 & z.Age < 75))
+            {
+                this.AccessScooter = true;
+            }
+        }
+        public void Info()
+        {
+            if (this.AccessCar == true & this.AccessPlane == true & this.AccessMotorBike == true & this.AccessBike == true & this.AccessScooter == true)
+            {
+                Console.WriteLine("Car");
+            }
+            if (this.AccessPlane == true)
+            {
+                Console.WriteLine("Plane");
+            }
+            if (this.AccessMotorBike == true)
+            {
+                Console.WriteLine("MotorBike");
+            }
+            if (this.AccessBike == true)
+            {
+                Console.WriteLine("Bike");
+            }
+            if (this.AccessScooter == true)
+            {
+                Console.WriteLine("Scooter");
+            }
+
+        }
+
+    }
+}
